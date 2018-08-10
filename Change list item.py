6ade58ea7441e2d573c4ccc654f7e9eeeadb34b8
Molly Zhu <mollyying566@gmail.com>
2018-08-10 09:41:14 +0800

    TEST

---
+++ Change list item.py
@@ -0,0 +1,12 @@
+spam=['a','b','c','d']
+
+def dot(p):
+    for i in range(len(p)):
+        if i+1<len(p):
+            p[i]=p[i]+", "
+        else:
+            p[i]="and "+p[i]
+        
+dot(spam)
+print(spam)
+        
