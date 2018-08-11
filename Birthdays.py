0b80b19734da8ce44e3fbc17416d35e7ab1abde5
mumymoon <mollyying566@gmail.com>
2018-08-11 13:09:24 +0800

    Add Birthdays.py

---
+++ Birthdays.py
@@ -0,0 +1,14 @@
+birthdays={'dad':'lunar 1024','mom':'lunar 0102','grandma':'lunar 1108','auntie':'lunar 1123','little sister':'lunar 1016','qing':'lunar 0317'
+           ,'gongling':'lunar 321','yancai':'gregorian 0823','passby':'lunar 0213'}
+while True:
+    name=input()
+    if name=='':
+        break
+    if name in birthdays:
+        print(birthdays[name]+ ' is the brithday of '+name)
+    else:
+        print('I do not know his birthday')
+        newbirthday=input()
+        birthdays[name]=newbirthday
+        print('birthday datebase updated')
+    
